3833c7749bf9729b8cd8a02b1fa49652359d5add
sarahandressa <sarahagdiaz@yahoo.com.br>
2025-02-18 00:44:15 -0300

    siteplan project

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,28 @@
+
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('active');
+});
+
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('active');
+});
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    });
+});
